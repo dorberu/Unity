f4602ade1529e7f122ea487debc9a9dd0c9863fb
Yuji Matsuda <dorberu@yahoo.co.jp>
2015-08-29 20:17:52 +0900

    Chapter02を実装

---
+++ Chapter02/Lesson01/MiniGame/Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float jumpPower;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Jump") || Input.touchCount > 0) {
+			GetComponent<Rigidbody>().velocity = new Vector3(0, jumpPower, 0);
+		}
+	}
+
+	void OnCollisionEnter (Collision other) {
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
