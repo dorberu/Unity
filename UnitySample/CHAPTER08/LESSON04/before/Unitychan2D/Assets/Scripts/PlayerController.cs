aa64804c4553b7105321c7fef061ad91ecd44af3
Yuji Matsuda <dorberu@yahoo.co.jp>
2015-11-07 14:13:27 +0900

    Sampleを追加

---
+++ UnitySample/CHAPTER08/LESSON04/before/Unitychan2D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//AudioSourceを必要とする
+//これを記述しておくとこのコンポーネントを追加した際に
+//一緒にAudioSorceコンポーネントも追加される
+[RequireComponent(typeof(AudioSource))]
+
+/*
+ * Playerクラス
+ * 
+ * UnityChan2DController以外のことを制御する
+ * 
+ */
+public class PlayerController : MonoBehaviour {
+	
+	
+	public AudioClip jumpVoice;  //ジャンプした時の声
+	public AudioClip damageVoice; //ダメージを受けた時雄の声
+	
+	private AudioSource mAudio;  //AudioSource
+	
+	/*
+    * はじめに呼ばれる関数
+    */
+	void Start () {
+		mAudio = GetComponent<AudioSource> ();
+	}
+	
+	/*
+     * ダメージを受けた時に呼ばれる関数
+     */
+	void OnDamage(){
+		//声を出す
+		PlayerVoice (damageVoice);
+	}
+	
+	void Jump(){
+		//声を出す
+		PlayerVoice (jumpVoice);
+	}
+	
+	void PlayerVoice(AudioClip clip){
+		//音を消す
+		mAudio.Stop ();
+		//音を再生する
+		mAudio.PlayOneShot (clip);
+	}
+}
---
+++ UnitySample/CHAPTER08/LESSON08/before/Unitychan2D/Assets/Scripts/Game.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+/*
+*ゲームの遷移を管理するクラス
+*/
+public class Game : MonoBehaviour {
+	// Gameクラスの唯一のインスタンス
+	private static Game mInstance;
+	/*Gameのインスタンスを返すパブリックな関数
+	*(staticでpublicなので、どのソースコードからでも呼ぶことができる)
+	*/
+	public static Game instance{
+		
+		get{
+			//インスタンスが参照されているか
+			if(mInstance == null){
+				//インスタンスを探し、参照する
+				mInstance = FindObjectOfType<Game>();
+			}
+			//インスタンスを返す
+			return mInstance;
+		}
+	}
+	//ゲームの状態
+	public enum STATE{
+		NONE, //何もない状態
+		START, //スタートの状態
+		MOVE, //ゲーム中の状態
+		GAMEOVER //ゲームオーバーの状態
+	};
+	//ゲームの状態
+	public STATE state{
+		get;
+		set;
+	}
+	
+	private Text mText;
+	
+	/*
+	*はじめに呼ばれる関数
+	*/
+	void Start () {
+		
+		mText = GetComponent<Text> ();
+		
+		//ゲームの状態をスタートに
+		state = STATE.START;
+		//StartCountDownを呼び出す
+		StartCoroutine("StartCountDown");
+	}
+	
+	/*
+	*毎回呼ばれる関数
+	*/
+	void Update () {
+		switch(state){
+		case STATE.START:
+			break;
+		case STATE.MOVE:
+			break;
+		case STATE.GAMEOVER:
+			//GUIにGame Overと表示する
+			mText.text = "Game Over";
+			//もし、Jumpボタンが押されたら
+			if(Input.GetButtonDown ("Jump")){
+				//今いるシーン
+				int currentScene = Application.loadedLevel;
+				//今いるシーンをもう一度最初から呼び出す
+				Application.LoadLevel (currentScene);
+			}
+			break;
+		}
+	}
+	
+	IEnumerator StartCountDown(){
+		//GUIの表示を3にする
+		mText.text = "3";
+		//1秒待つ
+		yield return new WaitForSeconds(1.0f);
+		//GUIの表示を3にする
+		mText.text = "2";
+		//1秒待つ
+		yield return new WaitForSeconds(1.0f);
+		//GUIの表示を3にする
+		mText.text = "1";
+		//1秒待つ
+		yield return new WaitForSeconds(1.0f);
+		//GUIに何も表示しない
+		mText.text = "";
+		//ゲームの状態をゲーム中にする
+		state = STATE.MOVE;
+	}
+}
